e5131e99f837f0c92715a8e5890283afa0a77c2a
Nicoletr <nicole.trpevski@gmail.com>
2021-09-14 17:29:32 +0800

    Created a framework for the JS file

---
+++ assets/script/script.js
@@ -0,0 +1,16 @@
+//variables to track
+indexofthequestion 
+time
+
+//variables to get elements
+document.getlelementId
+
+starButton.onClick = beginQuiz
+
+function beginQuiz(){
+	//hide the start screen
+	//unhide the question section
+	//start timer in JS
+	//showing timer in html
+	
+}
