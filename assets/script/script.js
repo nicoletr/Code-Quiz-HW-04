e552a94ad853ebb33acac75bfaddbc46e51bdc40
Nicoletr <nicole.trpevski@gmail.com>
2021-09-15 16:06:46 +0800

    Added highscores page in HTML and added variables in JS

--- assets/script/script.js
+++ assets/script/script.js
@@ -20,3 +20,36 @@ function beginQuiz(){
 }
 
 function nextQuestion();
+
+//Variables
+var timeLeft = document.getElementById("timer")
+var startButton = document.getElementById("start-button")
+var startPage = document.getElementById("start-page")
+
+var question1 = document.getElementById("question-1")
+var question1Correct = document.getElementById("question1-correct")
+var question1Wrong = document.getElementById("question1-wrong")
+
+var question2 = document.getElementById("question-2")
+var question2Correct = document.getElementById("question2-correct")
+var question2Wrong = document.getElementById("question2-wrong")
+
+var question3 = document.getElementById("question-3")
+var question3Correct = document.getElementById("question3-correct")
+var question3Wrong = document.getElementById("question3-wrong")
+
+var question4 = document.getElementById("question-4")
+var question4Correct = document.getElementById("question4-correct")
+var question4Wrong = document.getElementById("question4-wrong")
+
+var question5 = document.getElementById("question-5")
+var question5Correct = document.getElementById("question5-correct")
+var question5Wrong = document.getElementById("question5-wrong")
+
+var question6 = document.getElementById("question-6")
+var question6Correct = document.getElementById("question6-correct")
+var question6Wrong = document.getElementById("question6-wrong")
+
+var endPage = document.getElementById("end-page")
+
+var highscoresPage = document.getElementById("highscores-page")
